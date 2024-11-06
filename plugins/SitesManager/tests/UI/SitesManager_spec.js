21b31dbceaea1622532e913c516195246eec83cf
caddoo <matthew.caddoo@gmail.com>
2024-11-06 17:57:06 +1300

    Restore commented out UI tests

--- plugins/SitesManager/tests/UI/SitesManager_spec.js
+++ plugins/SitesManager/tests/UI/SitesManager_spec.js
@@ -41,65 +41,65 @@ describe("SitesManager", function () {
         await (await page.jQuery('.SitesManager .search:first .search_ico')).click();
     }
 
-    // it("should load correctly and show page 0", async function() {
-    //     await assertScreenshotEquals("loaded", async function () {
-    //         await page.goto(url);
-    //     });
-    // });
-    //
-    // it("should show page 1 when clicking next", async function() {
-    //     await assertScreenshotEquals("page_1", async function () {
-    //         await loadNextPage();
-    //     });
-    // });
-    //
-    // it("should show page 2 when clicking next", async function() {
-    //     await assertScreenshotEquals("page_2", async function () {
-    //         await loadNextPage();
-    //     });
-    // });
-    //
-    // it("should show page 1 when clicking prev", async function() {
-    //     await assertScreenshotEquals("page_1_again", async function () {
-    //         await loadPreviousPage();
-    //     });
-    // });
-    //
-    // it("should search for websites and reset page to 0", async function() {
-    //     await assertScreenshotEquals("search", async function () {
-    //         await searchForText('SiteTes');
-    //     });
-    // });
-    //
-    // it("should page within search result to page 1", async function() {
-    //     await assertScreenshotEquals("search_page_1", async function () {
-    //         await loadNextPage();
-    //     });
-    // });
-    //
-    // it("should search for websites no result", async function() {
-    //     await assertScreenshotEquals("search_no_result", async function () {
-    //         await searchForText('RanDoMSearChTerm');
-    //     });
-    // });
-    //
-    // it("should load the global settings page", async function() {
-    //     await assertScreenshotEquals("global_settings", async function () {
-    //         await page.goto('?module=SitesManager&action=globalSettings&idSite=1&period=day&date=yesterday&showaddsite=false');
-    //         await page.evaluate(function () {
-    //             $('.form-help:contains(UTC time is)').hide();
-    //         });
-    //     }, "h2:contains(Global websites settings)");
-    // });
-    //
-    // it("should be able to open and edit a site directly based on url parameter", async function() {
-    //     await assertScreenshotEquals("site_edit_url", async function () {
-    //         await page.goto(url + '#/editsiteid=23');
-    //         await page.evaluate(function () {
-    //             $('.form-help:contains(UTC time is)').hide();
-    //         });
-    //     });
-    // });
+    it("should load correctly and show page 0", async function() {
+        await assertScreenshotEquals("loaded", async function () {
+            await page.goto(url);
+        });
+    });
+
+    it("should show page 1 when clicking next", async function() {
+        await assertScreenshotEquals("page_1", async function () {
+            await loadNextPage();
+        });
+    });
+
+    it("should show page 2 when clicking next", async function() {
+        await assertScreenshotEquals("page_2", async function () {
+            await loadNextPage();
+        });
+    });
+
+    it("should show page 1 when clicking prev", async function() {
+        await assertScreenshotEquals("page_1_again", async function () {
+            await loadPreviousPage();
+        });
+    });
+
+    it("should search for websites and reset page to 0", async function() {
+        await assertScreenshotEquals("search", async function () {
+            await searchForText('SiteTes');
+        });
+    });
+
+    it("should page within search result to page 1", async function() {
+        await assertScreenshotEquals("search_page_1", async function () {
+            await loadNextPage();
+        });
+    });
+
+    it("should search for websites no result", async function() {
+        await assertScreenshotEquals("search_no_result", async function () {
+            await searchForText('RanDoMSearChTerm');
+        });
+    });
+
+    it("should load the global settings page", async function() {
+        await assertScreenshotEquals("global_settings", async function () {
+            await page.goto('?module=SitesManager&action=globalSettings&idSite=1&period=day&date=yesterday&showaddsite=false');
+            await page.evaluate(function () {
+                $('.form-help:contains(UTC time is)').hide();
+            });
+        }, "h2:contains(Global websites settings)");
+    });
+
+    it("should be able to open and edit a site directly based on url parameter", async function() {
+        await assertScreenshotEquals("site_edit_url", async function () {
+            await page.goto(url + '#/editsiteid=23');
+            await page.evaluate(function () {
+                $('.form-help:contains(UTC time is)').hide();
+            });
+        });
+    });
 
   it("the UI should change depending on what exclusion type gets selected", async function() {
     await page.goto('?module=SitesManager&action=globalSettings');
